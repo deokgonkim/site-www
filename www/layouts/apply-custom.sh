627a2f631fac0ca75240737dda65aa212c492357
Deokgon Kim <dgkim@dgkim.net>
2022-08-05 09:40:03 +0900

    debugging

--- www/layouts/apply-custom.sh
+++ www/layouts/apply-custom.sh
@@ -5,10 +5,11 @@
 #export THEME_PATH=../themes/geekblog
 
 export ENVS='$GOOGLE_ADS_ID'
+echo "Using $GOOGLE_ADS_ID"
 
 export FILES="./partials/head/custom.html.tpl"
 
 for FILE in $FILES
 do
-  cat $FILE | envsubst "$ENVS" > ./${FILE%.tpl}
+  cat $FILE | envsubst "$ENVS" | tee ./${FILE%.tpl}
 done
